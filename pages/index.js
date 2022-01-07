2d5b49ef8ee61dc7c8029e48be7ffe5552260a73
emmartin <emmartin@paradigmadigital.com>
2022-01-07 20:58:02 +0100

    style home page

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,37 @@
+/* eslint-disable @next/next/no-img-element */
 import Button from "../components/ui/button";
-
-function HomePage() {
+import styles from "../styles/home.module.css";
+function Home() {
   const picListLink = "/list";
   return (
-    <div>
-      <header>
-        <h1>CoolPics</h1>
+    <div className={styles.container}>
+      <header className={styles.header}>
+        <h1 className={styles.headerTitle}>CoolPics</h1>
       </header>
-      <main>
-        <h2>Are you bored?</h2>
-        <div>
-          <p>
+      <main className={styles.main}>
+        <div className={styles.ctaContainer}>
+          <h2 className={styles.ctaTitle}>Are you bored?</h2>
+
+          <p className={styles.ctaText}>
             CoolPics helps you to spend hours of your day scrolling down and
             sharing a big list of random images.
           </p>
+
+          <Button href={picListLink} class="start">
+            {"Start here".toUpperCase()}
+          </Button>
         </div>
-        <Button href={picListLink} class="start">
-          {"Start here".toUpperCase()}
-        </Button>
+        <div className={styles.imgContainer}>
+          <img
+            className={styles.img}
+            src="/perrete.jpg"
+            alt="a doggy covered with a blanket sitting in a forest path"
+          />
+          <div className={styles.square}></div>
+        </div>
       </main>
     </div>
   );
 }
 
-export default HomePage;
+export default Home;
